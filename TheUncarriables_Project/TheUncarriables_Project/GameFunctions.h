aa7de5ad19b986149956f8061305da875dfbaa24
DGBratoevski20 <DGBratoevski20@codingburgas.bg>
2021-11-26 20:28:49 +0200

    Add GameFunction.cpp and GameFunction.h

---
+++ TheUncarriables_Project/TheUncarriables_Project/GameFunctions.h
@@ -0,0 +1,25 @@
+#pragma once
+#include<SDL.h> // include SDL
+#include<iostream>
+#include<stdlib.h>
+#include<cmath>
+#include<time.h>
+#include<string>
+#include<SDL_image.h> // Here we can use images in SDL
+using namespace std;
+
+
+void CreateWindow();
+void SetThePoint();
+void SetBubbleLeft();
+void SetBubbleRight();
+void SetTextureAndSurfaceOfPoint();
+void SetTextureAndSurfaceOfDiver();
+void SetTextureAndSurfaceOfBubbleleft();
+void SetTextureAndSurfaceOfBubbleright();
+void SetDiver();
+bool GameOver(float gameover);
+bool FindCollusion();
+void Quiz();
+void MainLoop();
+void Setting();
---
+++ TheUncarriables_Project/UnitTesting/UnitTesting.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "../TheUncarriables_Project/GameFunctions.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(UnitTesting)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
