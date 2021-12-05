26a38b491d404d49e82d95fcf5a322791e851691
ipstoyanov20 <ipstoyanov20@codingburgas.bg>
2021-12-05 23:05:40 +0200

    Add Bombs into game

--- TheUncarriables_Project/TheUncarriables_Project/GameFunctions.cpp
+++ TheUncarriables_Project/TheUncarriables_Project/GameFunctions.cpp
@@ -6,12 +6,16 @@
 #include<string>
 #include "../TheUncarriables_Project/GameFunctions.h"
 #include<SDL_image.h> // Here we can use images in SDL
-#include<SDL_ttf.h>
+#include<SDL_ttf.h>//Here we can display text in SDL window
 using namespace std;
 
 SDL_Window* window;
 SDL_Renderer* renderer;
 SDL_Rect diver;
+SDL_Surface* surfaceOfBombOne;
+SDL_Texture* textureOfBombOne;
+SDL_Surface* surfaceOfBombTwo;
+SDL_Texture* textureOfBombTwo;
 SDL_Event SDLevent;
 SDL_Surface* surfaceOfBubbleLeft;
 SDL_Texture* textureOfBubbleLeft;
@@ -26,7 +30,8 @@ SDL_Rect bubbleLeft;
 SDL_Rect bubbleRight;
 SDL_Rect scoreRect;
 TTF_Font* font = nullptr;
-
+SDL_Rect BombOne;
+SDL_Rect BombTwo;
 SDL_Color color = { 0,0,0 };
 SDL_Surface* scoreSurface = nullptr;
 SDL_Texture* scoreTexture = nullptr;
@@ -35,17 +40,25 @@ float LeftPosOfDiver;
 float PosOfPoint;
 float RightPosOfDiver;
 float y = 0;
+float DecreasePosOfPoint = 0.25;
 float x = 0;
+float IncreaseSpeedOfDiver = 0.5;
+int counterOfDecrease = 0;
 float BubbleLeftY;
 float BubbleRightY;
 bool playing = true;
+float decreaseScore = 0.08;
 float gameover = 800;
 string answer;
 int counterOfArrayForQuestion = 0;
 int fillerArrayForQuestion = 0;
 int randomquiz = 0;
+float PosYOfBombOne;
+float PosYOfBombTwo;
+bool BoolOne = false;
+bool BoolTwo = false;
 int randomQuestion = 0;
-int ArrayForQuestion[10] = {-1, -1, -1, -1, -1, -1, -1, -1, -1 ,-1};
+int ArrayForQuestion[10] = { -1, -1, -1, -1, -1, -1, -1, -1, -1 ,-1 };
 
 string arrayOfQuestion[10] = { "In what unit are forces measured?", "When the net force on an object equals 0 Newtons, we know the forces are __?", "Anytime the net force does NOT equal zero, the forces are __", "What is the scientific name for a push or a pull?", "The combination of multiple forces acting on an object is known as", "Which type of force creates contact between two surfaces?", "What operation is used to calculate net forces acting in the SAME direction?", "What operation is used to calculate net forces acting in DIFFERENT directions?", "True or false, in physics we measure speed in m/s", "True or false, Forces cannot act on objects from a distance, only through direct contact." };
 void CreateWindow()
@@ -72,7 +85,7 @@ void SetscoreRect()
 	scoreRect.w = 60;
 	scoreRect.y = 0;
 	font = TTF_OpenFont("arial.ttf", 25);
-	
+
 }
 void UpdateScore(float gameover)
 {
@@ -140,11 +153,15 @@ bool GameOver(float gameover)
 		return playing;
 	}
 }
-bool FindCollision()
+bool FindCollisionOfPoint()
 {
-	if (((round(point.y) - round(diver.y) >= 0) && (round(point.y) - round(diver.y) <= 35) && (round(point.x) - round(diver.x) <= 20) && (round(point.x) - round(diver.x) >= -20)))
+	if (((round(point.y) - round(diver.y) >= -35) && (round(point.y) - round(diver.y) <= 35) && (round(point.x) - round(diver.x) <= 21) && (round(point.x) - round(diver.x) >= -21)))
 	{
 		return true;
+		if (round(point.y) - round(point.y) == -35 && round(point.x) - round(diver.x) >= -17)
+		{
+			return true;
+		}
 	}
 
 }
@@ -156,7 +173,9 @@ void Rendering()
 	SDL_RenderCopy(renderer, textureOfDiver, 0, &diver);
 	//Copy textureOfBubbleLeft in bubbleLeft rectangle
 	SDL_RenderCopy(renderer, textureOfBubbleLeft, 0, &bubbleLeft);
-	
+
+	SDL_RenderCopy(renderer, textureOfBombOne, 0, &BombOne);
+	SDL_RenderCopy(renderer, textureOfBombTwo, 0, &BombTwo);
 	//Copy textureOfBubbleRight in bubbleRight rectangle
 	SDL_RenderCopy(renderer, textureOfBubbleRight, 0, &bubbleRight);
 	//Copy scoreTexture in scoreRect rectangle
@@ -170,7 +189,7 @@ void Rendering()
 }
 void Random()
 {
-	
+
 	randomQuestion = rand() % 10;
 	if (ArrayForQuestion[counterOfArrayForQuestion] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 1] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 2] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 3] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 4] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 5] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 6] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 7] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 8] == randomQuestion || ArrayForQuestion[counterOfArrayForQuestion + 9] == randomQuestion)
 	{
@@ -178,12 +197,30 @@ void Random()
 		Random();
 	}
 }
+void SetBombOne()
+{
+	BombOne.x = 200;
+	BombOne.y = 500;
+	BombOne.w = 30;
+	BombOne.h = 30;
+	surfaceOfBombOne = IMG_Load("bomb.png");
+	textureOfBombOne = SDL_CreateTextureFromSurface(renderer, surfaceOfBombOne);
+}
+void SetBombTwo()
+{
+	BombTwo.x = 300;
+	BombTwo.y = 500;
+	BombTwo.w = 30;
+	BombTwo.h = 30;
+	surfaceOfBombTwo = IMG_Load("bomb.png");
+	textureOfBombTwo = SDL_CreateTextureFromSurface(renderer, surfaceOfBombTwo);
+}
 void Quiz()
 {
 	srand(time(0));
-	
+
 	cout << endl << endl << "Type only with small letters:" << endl;
-	
+
 	Random();
 	cout << arrayOfQuestion[randomQuestion] << endl;
 
@@ -196,7 +233,7 @@ void Quiz()
 			cout << "Correct" << endl;
 			gameover += 150;
 			cout << "You increase your score" << endl;
-			
+
 		}
 		else {
 			cout << "Wrong" << endl << "The correct answer is \'newtons\'" << endl;
@@ -212,13 +249,13 @@ void Quiz()
 			cout << "Correct" << endl;
 			gameover += 150;
 			cout << "You increase your score" << endl;
-			
+
 		}
 		else {
 			cout << "Wrong" << endl << "The correct answer is \'balanced\'" << endl;
 			gameover -= 110;
 			cout << "You decrease you score" << endl;
-			
+
 		}
 		break;
 	}
@@ -228,7 +265,7 @@ void Quiz()
 			cout << "Correct" << endl;
 			gameover += 150;
 			cout << "You increase your score" << endl;
-			
+
 
 		}
 		else {
@@ -286,7 +323,7 @@ void Quiz()
 			cout << "Correct" << endl;
 			gameover += 150;
 			cout << "You increase your score" << endl;
-			
+
 		}
 		else {
 			cout << "Wrong" << endl << "The correct answer is \'+\'" << endl;
@@ -340,28 +377,237 @@ void Quiz()
 
 	}
 	UpdateScore(gameover);
-	
+
 	ArrayForQuestion[fillerArrayForQuestion] = randomQuestion;
 	fillerArrayForQuestion++;
+
+}
+void RenderDiver()
+{
+	//Copy textureOfDiver in diver rectangle
+	SDL_RenderCopy(renderer, textureOfDiver, 0, &diver);
+
+
+}
+void FindKeys()
+{
+
+	if (SDLevent.type == SDL_KEYDOWN)
+	{
+		switch (SDLevent.key.keysym.sym)
+		{
+		case SDLK_LEFT:
+		{
+			LeftPosOfDiver -= IncreaseSpeedOfDiver;
+			diver.x = LeftPosOfDiver;
+			RightPosOfDiver = LeftPosOfDiver;
+
+
+
+
+			RenderDiver();
+			break;
+		}
+		case SDLK_RIGHT:
+		{
+			RightPosOfDiver += IncreaseSpeedOfDiver;
+			diver.x = RightPosOfDiver;
+			LeftPosOfDiver = RightPosOfDiver;
+
+
+
+
+			RenderDiver();
+			break;
+		}
+		}
+	}
+}
+void RandomPosOfThePoint()
+{
+	if (FindCollisionOfPoint() || point.y < -30)
+	{
+		if (FindCollisionOfPoint())
+		{
+			gameover += 200;
+			randomquiz++;
+			if (randomquiz % 5 == 0)
+			{
+
+				if (randomquiz >= 50)
+				{
+					if (randomquiz == 50)
+					{
+						cout << endl << endl << "You Pass the Quiz!! Continue to play" << endl << endl;
+						
+						SDL_Delay(2000);
+
+					}
+					else
+					{
+						if (randomquiz % 5 == 0)
+						{
+							if (counterOfDecrease >= 2)
+							{
+
+							}
+							else {
+								DecreasePosOfPoint += 0.07;
+								IncreaseSpeedOfDiver = 0.7;
+								decreaseScore = 0.1;
+							}
+
+							counterOfDecrease++;
+
+						}
+					}
+
+				}
+				else {
+					gameover -= 200;
+					Quiz();
+					cout << "Press \'Alt + Tab\' and WAIT to continue playing" << endl;
+					SDL_Delay(4000);
+					cout << "Ready?" << endl;
+					SDL_Delay(2000);
+					cout << "GOO!";
+					SDL_Delay(1000);
+					//clear console
+					system("cls");
+				}
+				
+
+			}
+		}
+
+		y = rand() % 20 + 500;
+		randomagain:
+		x = rand() % 520 + 55;
+
+		if ((x >= BombOne.x - 50 && x <= BombOne.x) || (x <= BombOne.x + 50 && x >= BombOne.x) || (x >= BombTwo.x - 50 && x <= BombTwo.x) || (x <= BombTwo.x + 50 && x >= BombTwo.x))
+		{
+			goto randomagain;
+		}
+		else {
+			point.x = x;
+		}
+		
+		point.y = y;
+		PosOfPoint = point.y;
+
+	}
+}
+bool FindCollisionOfBombOne() 
+{
+	if (((round(BombOne.y) - round(diver.y) >= -35) && (round(BombOne.y) - round(diver.y) <= 35) && (round(BombOne.x) - round(diver.x) <= 20) && (round(BombOne.x) - round(diver.x) >= -20)))
+	{
+		return true;
+		
+		if (round(BombOne.y) - round(diver.y) == -35 && round(BombOne.x) - round(diver.x) >= -17)
+		{
+			return true;
+			
+			
+		}
+	}
 	
+	
+}
+bool FindCollisionOfBombTwo()
+{
+	if (((round(BombTwo.y) - round(diver.y) >= -35) && (round(BombTwo.y) - round(diver.y) <= 35) && (round(BombTwo.x) - round(diver.x) <= 20) && (round(BombTwo.x) - round(diver.x) >= -20)))
+	{
+	return true;
+	
+	if (round(BombTwo.y) - round(diver.y) == -35 && round(BombTwo.x) - round(diver.x) >= -17)
+	{
+		return true;
+		
+	}
+	}
+}
+void RandomPosOfBombs()
+{
+	if (FindCollisionOfBombOne() || BombOne.y < -60 || BombTwo.y < -60 || FindCollisionOfBombTwo())
+	{
+		
+		
+		if (BombOne.y < -60)
+		{
+			
+			x = rand() % 520 + 55;
+			
+
+			BombOne.x = x;
+			
+			
+
+		}
+		else if (BombTwo.y < -60)
+		{
+			
+			x = rand() % 520 + 55;
+
+			BombTwo.x = x;
+			
+		}
+		else if(FindCollisionOfBombOne())
+		{
+			
+			
+				gameover -= 100;
+				x = rand() % 520 + 55;
+				y = rand() % 20 + 500;
+				BombOne.y = y;
+				BombOne.x = x;
+				PosYOfBombOne = BombOne.y;
+			
+			
+				
+			
+			
+			
+			
+			
+		}
+		else if (FindCollisionOfBombTwo())
+		{
+			gameover -= 100;
+			x = rand() % 520 + 55;
+			y = rand() % 20 + 500;
+			BombTwo.y = y;
+			BombTwo.x = x;
+			PosYOfBombTwo = BombTwo.y;
+		}
+		
+		
+		
+		
+		
+		
+		
+
+
+	}
 }
 void MainLoop()
 {
+
 	LeftPosOfDiver = diver.x;
 	PosOfPoint = point.y;
 	RightPosOfDiver = diver.x;
-
+	PosYOfBombOne = BombOne.y;
+	PosYOfBombTwo = BombTwo.y;
 	BubbleLeftY = bubbleLeft.y;
 	BubbleRightY = bubbleRight.y;
-
 
 	while (playing)
 	{
 
 		SDL_PollEvent(&SDLevent);
-		
-	
-		if (SDLevent.type == SDL_QUIT || diver.x == 0 || diver.x == 700)
+
+
+		if (SDLevent.type == SDL_QUIT)
 		{
 
 			cout << endl << "GoodBye";
@@ -369,51 +615,20 @@ void MainLoop()
 
 
 		}
-		if (SDLevent.type == SDL_KEYDOWN)
-		{
-			switch (SDLevent.key.keysym.sym)
-			{
-			case SDLK_LEFT:
-			{
-				LeftPosOfDiver -= 0.5;
-				diver.x = LeftPosOfDiver;
-				RightPosOfDiver = LeftPosOfDiver;
 
+		FindKeys();
 
-
-
-				goto GoToRenderAll;
-				break;
-			}
-			case SDLK_RIGHT:
-			{
-				RightPosOfDiver += 0.5;
-				diver.x = RightPosOfDiver;
-				LeftPosOfDiver = RightPosOfDiver;
-
-
-
-
-				goto GoToRenderAll;
-				break;
-			}
-			}
-		}
-
-
-
-	GoToRenderAll:
 		Rendering();
-		
 
-
-
-
-		PosOfPoint -= 0.25;
+		PosOfPoint -= DecreasePosOfPoint;
+		PosYOfBombOne -= 0.3;
+		PosYOfBombTwo -= 0.2;
 		BubbleLeftY -= 0.15;
 		BubbleRightY -= 0.08;
 		bubbleLeft.y = BubbleLeftY;
 		point.y = PosOfPoint;
+		BombOne.y = PosYOfBombOne;
+		BombTwo.y = PosYOfBombTwo;
 		bubbleRight.y = BubbleRightY;
 
 
@@ -422,53 +637,37 @@ void MainLoop()
 			BubbleLeftY = 500;
 
 		}
-		if (bubbleRight.y < -60)
+		else if (bubbleRight.y < -60)
 		{
 			BubbleRightY = 500;
 
 		}
-		//Find collision and set random position of the point
-		if (FindCollision() || point.y < -30)
+		else if (BombOne.y < -60)
 		{
-			if (FindCollision())
-			{
-				gameover += 200;
-				randomquiz++;
-				if (randomquiz % 5 == 0)
-				{
-					
-					if (randomquiz >= 50)
-					{
-						if (randomquiz == 50)
-						{
-							cout << endl << endl << "You Pass the Quiz!! Continue to play" << endl << endl;
-							SDL_Delay(2000);
-						}
-						continue;
-					}
-					else {
-						gameover -= 200;
-						Quiz();
-						cout << "Press \'Alt + Tab\' and WAIT to continue playing" << endl;
-						SDL_Delay(4000);
-						cout << "Ready?" << endl;
-						SDL_Delay(2000);
-						cout << "GOO!";
-						SDL_Delay(1000);
-					}
-					
-
-				}
-			}
-
-			y = rand() % 20 + 500;
-			x = rand() % 520 + 55;
-			point.x = x;
-			point.y = y;
-			PosOfPoint = point.y;
+			PosYOfBombOne = 500;
+		}
+		else if (BombTwo.y < -60)
+		{
+			PosYOfBombTwo = 500;
+		}
+		else if (diver.x < -10)
+		{
+			diver.x = 700;
+			LeftPosOfDiver = diver.x;
+			RightPosOfDiver = diver.x;
 
 		}
-		gameover -= 0.08;
+		else if (diver.x > 710)
+		{
+			diver.x = 0;
+			LeftPosOfDiver = diver.x;
+			RightPosOfDiver = diver.x;
+		}
+
+		//Find collision and set random position of the point
+		RandomPosOfThePoint();
+		RandomPosOfBombs();
+		gameover -= decreaseScore;
 		UpdateScore(gameover);
 		// set Game Over when score is less or equal to 0
 		if (GameOver(gameover) == false)
@@ -477,12 +676,15 @@ void MainLoop()
 		}
 	}
 }
+
 void Setting()
 {
 	SetThePoint();
 	SetDiver();
 	SetBubbleLeft();
 	SetBubbleRight();
+	SetBombTwo();
+	SetBombOne();
 	SetscoreRect();
 	SetTextureAndSurfaceOfPoint();
 	SetTextureAndSurfaceOfDiver();
