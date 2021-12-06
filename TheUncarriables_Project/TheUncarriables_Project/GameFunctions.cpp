2ad44ae21251742d5ce9d6e16ffa30e10630bf56
iatsrangalov20 <84869233+iatsrangalov20@users.noreply.github.com>
2021-12-06 15:02:21 +0200

    Add sdlProperties namespace

--- TheUncarriables_Project/TheUncarriables_Project/GameFunctions.cpp
+++ TheUncarriables_Project/TheUncarriables_Project/GameFunctions.cpp
@@ -7,35 +7,43 @@
 #include "../TheUncarriables_Project/GameFunctions.h"
 #include<SDL_image.h> // Here we can use images in SDL
 #include<SDL_ttf.h>//Here we can display text in SDL window
+
 using namespace std;
 
-SDL_Window* window;
-SDL_Renderer* renderer;
-SDL_Rect diver;
-SDL_Surface* surfaceOfBombOne;
-SDL_Texture* textureOfBombOne;
-SDL_Surface* surfaceOfBombTwo;
-SDL_Texture* textureOfBombTwo;
-SDL_Event SDLevent;
-SDL_Surface* surfaceOfBubbleLeft;
-SDL_Texture* textureOfBubbleLeft;
-SDL_Surface* surfaceOfBubbleRight;
-SDL_Texture* textureOfBubbleRight;
-SDL_Surface* surfaceOfDiver;
-SDL_Texture* textureOfDiver;
-SDL_Surface* surfaceOfPoint;
-SDL_Texture* textureOfPoint;
-SDL_Rect point;
-SDL_Rect bubbleLeft;
-SDL_Rect bubbleRight;
-SDL_Rect scoreRect;
-TTF_Font* font = nullptr;
-SDL_Rect BombOne;
-SDL_Rect BombTwo;
-SDL_Color color = { 0,0,0 };
-SDL_Surface* scoreSurface = nullptr;
-SDL_Texture* scoreTexture = nullptr;
-//Useful variables
+namespace sdlProperties
+{
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+	SDL_Rect diver;
+	SDL_Surface* surfaceOfBombOne;
+	SDL_Texture* textureOfBombOne;
+	SDL_Surface* surfaceOfBombTwo;
+	SDL_Texture* textureOfBombTwo;
+	SDL_Event SDLevent;
+	SDL_Surface* surfaceOfBubbleLeft;
+	SDL_Texture* textureOfBubbleLeft;
+	SDL_Surface* surfaceOfBubbleRight;
+	SDL_Texture* textureOfBubbleRight;
+	SDL_Surface* surfaceOfDiver;
+	SDL_Texture* textureOfDiver;
+	SDL_Surface* surfaceOfPoint;
+	SDL_Texture* textureOfPoint;
+	SDL_Rect point;
+	SDL_Rect bubbleLeft;
+	SDL_Rect bubbleRight;
+	SDL_Rect scoreRect;
+	TTF_Font* font = nullptr;
+	SDL_Rect BombOne;
+	SDL_Rect BombTwo;
+	SDL_Color color = { 0,0,0 };
+	SDL_Surface* scoreSurface = nullptr;
+	SDL_Texture* scoreTexture = nullptr;
+}
+
+
+
+
+
 float LeftPosOfDiver;
 float PosOfPoint;
 float RightPosOfDiver;
@@ -64,86 +72,86 @@ string arrayOfQuestion[10] = { "In what unit are forces measured?", "When the ne
 void CreateWindow()
 {
 
-	SDL_CreateWindowAndRenderer(700, 500, 0, &window, &renderer);
-	SDL_SetWindowTitle(window, "TheUncarriables");
-	SDL_SetWindowPosition(window, 700, SDL_WINDOWPOS_CENTERED);
+	SDL_CreateWindowAndRenderer(700, 500, 0, &sdlProperties::window, &sdlProperties::renderer);
+	SDL_SetWindowTitle(sdlProperties::window, "TheUncarriables");
+	SDL_SetWindowPosition(sdlProperties::window, 700, SDL_WINDOWPOS_CENTERED);
 }
 void SetThePoint()
 {
-	point.x = 100;
+	sdlProperties::point.x = 100;
 
-	point.y = 500;
+	sdlProperties::point.y = 500;
 
-	point.h = 30;
+	sdlProperties::point.h = 30;
 
-	point.w = 30;
+	sdlProperties::point.w = 30;
 }
 void SetscoreRect()
 {
-	scoreRect.x = 600;
-	scoreRect.h = 50;
-	scoreRect.w = 60;
-	scoreRect.y = 0;
-	font = TTF_OpenFont("arial.ttf", 25);
+	sdlProperties::scoreRect.x = 600;
+	sdlProperties::scoreRect.h = 50;
+	sdlProperties::scoreRect.w = 60;
+	sdlProperties::scoreRect.y = 0;
+	sdlProperties::font = TTF_OpenFont("arial.ttf", 25);
 
 }
 void UpdateScore(float gameover)
 {
 	int firstPartOfFloat = round(gameover);
 	string score = to_string(firstPartOfFloat);
-	scoreSurface = TTF_RenderText_Solid(font, score.c_str(), color);
-	scoreTexture = SDL_CreateTextureFromSurface(renderer, scoreSurface);
+	sdlProperties::scoreSurface = TTF_RenderText_Solid(sdlProperties::font, score.c_str(), sdlProperties::color);
+	sdlProperties::scoreTexture = SDL_CreateTextureFromSurface(sdlProperties::renderer, sdlProperties::scoreSurface);
 }
 void SetBubbleLeft()
 {
 
-	bubbleLeft.x = 10;
+	sdlProperties::bubbleLeft.x = 10;
 
-	bubbleLeft.y = 500;
+	sdlProperties::bubbleLeft.y = 500;
 
-	bubbleLeft.h = 50;
+	sdlProperties::bubbleLeft.h = 50;
 
-	bubbleLeft.w = 50;
+	sdlProperties::bubbleLeft.w = 50;
 }
 
 void SetBubbleRight()
 {
 
-	bubbleRight.x = 650;
+	sdlProperties::bubbleRight.x = 650;
 
-	bubbleRight.y = 500;
+	sdlProperties::bubbleRight.y = 500;
 
-	bubbleRight.h = 50;
+	sdlProperties::bubbleRight.h = 50;
 
-	bubbleRight.w = 50;
+	sdlProperties::bubbleRight.w = 50;
 }
 void SetTextureAndSurfaceOfPoint()
 {
-	surfaceOfPoint = IMG_Load("shield.png");
-	textureOfPoint = SDL_CreateTextureFromSurface(renderer, surfaceOfPoint);
+	sdlProperties::surfaceOfPoint = IMG_Load("shield.png");
+	sdlProperties::textureOfPoint = SDL_CreateTextureFromSurface(sdlProperties::renderer, sdlProperties::surfaceOfPoint);
 }
 void SetTextureAndSurfaceOfDiver()
 {
-	surfaceOfDiver = IMG_Load("diver.png");
-	textureOfDiver = SDL_CreateTextureFromSurface(renderer, surfaceOfDiver);
+	sdlProperties::surfaceOfDiver = IMG_Load("diver.png");
+	sdlProperties::textureOfDiver = SDL_CreateTextureFromSurface(sdlProperties::renderer, sdlProperties::surfaceOfDiver);
 }
 void SetTextureAndSurfaceOfBubbleleft()
 {
-	surfaceOfBubbleLeft = IMG_Load("bubble.png");
-	textureOfBubbleLeft = SDL_CreateTextureFromSurface(renderer, surfaceOfBubbleLeft);
+	sdlProperties::surfaceOfBubbleLeft = IMG_Load("bubble.png");
+	sdlProperties::textureOfBubbleLeft = SDL_CreateTextureFromSurface(sdlProperties::renderer, sdlProperties::surfaceOfBubbleLeft);
 }
 void SetTextureAndSurfaceOfBubbleright()
 {
-	surfaceOfBubbleRight = IMG_Load("bubble2.png");
-	textureOfBubbleRight = SDL_CreateTextureFromSurface(renderer, surfaceOfBubbleRight);
+	sdlProperties::surfaceOfBubbleRight = IMG_Load("bubble2.png");
+	sdlProperties::textureOfBubbleRight = SDL_CreateTextureFromSurface(sdlProperties::renderer, sdlProperties::surfaceOfBubbleRight);
 }
 void SetDiver()
 {
-	diver.h = 50;
-	diver.w = 30;
-	diver.y = 50;
+	sdlProperties::diver.h = 50;
+	sdlProperties::diver.w = 30;
+	sdlProperties::diver.y = 50;
 
-	diver.x = 200;
+	sdlProperties::diver.x = 200;
 }
 bool GameOver(float gameover)
 {
@@ -155,10 +163,10 @@ bool GameOver(float gameover)
 }
 bool FindCollisionOfPoint()
 {
-	if (((round(point.y) - round(diver.y) >= -35) && (round(point.y) - round(diver.y) <= 35) && (round(point.x) - round(diver.x) <= 21) && (round(point.x) - round(diver.x) >= -21)))
+	if (((round(sdlProperties::point.y) - round(sdlProperties::diver.y) >= -35) && (round(sdlProperties::point.y) - round(sdlProperties::diver.y) <= 35) && (round(sdlProperties::point.x) - round(sdlProperties::diver.x) <= 21) && (round(sdlProperties::point.x) - round(sdlProperties::diver.x) >= -21)))
 	{
 		return true;
-		if (round(point.y) - round(point.y) == -35 && round(point.x) - round(diver.x) >= -17)
+		if (round(sdlProperties::point.y) - round(sdlProperties::point.y) == -35 && round(sdlProperties::point.x) - round(sdlProperties::diver.x) >= -17)
 		{
 			return true;
 		}
@@ -168,24 +176,24 @@ bool FindCollisionOfPoint()
 void Rendering()
 {
 	//Copy textureOfPoint in point rectangle
-	SDL_RenderCopy(renderer, textureOfPoint, 0, &point);
+	SDL_RenderCopy(sdlProperties::renderer, sdlProperties::textureOfPoint, 0, &sdlProperties::point);
 	//Copy textureOfDiver in diver rectangle
-	SDL_RenderCopy(renderer, textureOfDiver, 0, &diver);
+	SDL_RenderCopy(sdlProperties::renderer, sdlProperties::textureOfDiver, 0, &sdlProperties::diver);
 	//Copy textureOfBubbleLeft in bubbleLeft rectangle
-	SDL_RenderCopy(renderer, textureOfBubbleLeft, 0, &bubbleLeft);
+	SDL_RenderCopy(sdlProperties::renderer, sdlProperties::textureOfBubbleLeft, 0, &sdlProperties::bubbleLeft);
 
-	SDL_RenderCopy(renderer, textureOfBombOne, 0, &BombOne);
-	SDL_RenderCopy(renderer, textureOfBombTwo, 0, &BombTwo);
+	SDL_RenderCopy(sdlProperties::renderer, sdlProperties::textureOfBombOne, 0, &sdlProperties::BombOne);
+	SDL_RenderCopy(sdlProperties::renderer, sdlProperties::textureOfBombTwo, 0, &sdlProperties::BombTwo);
 	//Copy textureOfBubbleRight in bubbleRight rectangle
-	SDL_RenderCopy(renderer, textureOfBubbleRight, 0, &bubbleRight);
+	SDL_RenderCopy(sdlProperties::renderer, sdlProperties::textureOfBubbleRight, 0, &sdlProperties::bubbleRight);
 	//Copy scoreTexture in scoreRect rectangle
-	SDL_RenderCopy(renderer, scoreTexture, 0, &scoreRect);
+	SDL_RenderCopy(sdlProperties::renderer, sdlProperties::scoreTexture, 0, &sdlProperties::scoreRect);
 	//Set Background color to the window
-	SDL_SetRenderDrawColor(renderer, 51, 153, 255, 255);
+	SDL_SetRenderDrawColor(sdlProperties::renderer, 51, 153, 255, 255);
 	//present renderer and visualisation of all shapes
-	SDL_RenderPresent(renderer);
+	SDL_RenderPresent(sdlProperties::renderer);
 	//Clear renderer for next position of all shapes
-	SDL_RenderClear(renderer);
+	SDL_RenderClear(sdlProperties::renderer);
 }
 void Random()
 {
@@ -199,21 +207,21 @@ void Random()
 }
 void SetBombOne()
 {
-	BombOne.x = 200;
-	BombOne.y = 500;
-	BombOne.w = 30;
-	BombOne.h = 30;
-	surfaceOfBombOne = IMG_Load("bomb.png");
-	textureOfBombOne = SDL_CreateTextureFromSurface(renderer, surfaceOfBombOne);
+	sdlProperties::BombOne.x = 200;
+	sdlProperties::BombOne.y = 500;
+	sdlProperties::BombOne.w = 30;
+	sdlProperties::BombOne.h = 30;
+	sdlProperties::surfaceOfBombOne = IMG_Load("bomb.png");
+	sdlProperties::textureOfBombOne = SDL_CreateTextureFromSurface(sdlProperties::renderer, sdlProperties::surfaceOfBombOne);
 }
 void SetBombTwo()
 {
-	BombTwo.x = 300;
-	BombTwo.y = 500;
-	BombTwo.w = 30;
-	BombTwo.h = 30;
-	surfaceOfBombTwo = IMG_Load("bomb.png");
-	textureOfBombTwo = SDL_CreateTextureFromSurface(renderer, surfaceOfBombTwo);
+	sdlProperties::BombTwo.x = 300;
+	sdlProperties::BombTwo.y = 500;
+	sdlProperties::BombTwo.w = 30;
+	sdlProperties::BombTwo.h = 30;
+	sdlProperties::surfaceOfBombTwo = IMG_Load("bomb.png");
+	sdlProperties::textureOfBombTwo = SDL_CreateTextureFromSurface(sdlProperties::renderer, sdlProperties::surfaceOfBombTwo);
 }
 void Quiz()
 {
@@ -385,21 +393,21 @@ void Quiz()
 void RenderDiver()
 {
 	//Copy textureOfDiver in diver rectangle
-	SDL_RenderCopy(renderer, textureOfDiver, 0, &diver);
+	SDL_RenderCopy(sdlProperties::renderer, sdlProperties::textureOfDiver, 0, &sdlProperties::diver);
 
 
 }
 void FindKeys()
 {
 
-	if (SDLevent.type == SDL_KEYDOWN)
+	if (sdlProperties::SDLevent.type == SDL_KEYDOWN)
 	{
-		switch (SDLevent.key.keysym.sym)
+		switch (sdlProperties::SDLevent.key.keysym.sym)
 		{
 		case SDLK_LEFT:
 		{
 			LeftPosOfDiver -= IncreaseSpeedOfDiver;
-			diver.x = LeftPosOfDiver;
+			sdlProperties::diver.x = LeftPosOfDiver;
 			RightPosOfDiver = LeftPosOfDiver;
 
 
@@ -411,7 +419,7 @@ void FindKeys()
 		case SDLK_RIGHT:
 		{
 			RightPosOfDiver += IncreaseSpeedOfDiver;
-			diver.x = RightPosOfDiver;
+			sdlProperties::diver.x = RightPosOfDiver;
 			LeftPosOfDiver = RightPosOfDiver;
 
 
@@ -425,7 +433,7 @@ void FindKeys()
 }
 void RandomPosOfThePoint()
 {
-	if (FindCollisionOfPoint() || point.y < -30)
+	if (FindCollisionOfPoint() || sdlProperties::point.y < -30)
 	{
 		if (FindCollisionOfPoint())
 		{
@@ -484,26 +492,26 @@ void RandomPosOfThePoint()
 		randomagain:
 		x = rand() % 520 + 55;
 
-		if ((x >= BombOne.x - 50 && x <= BombOne.x) || (x <= BombOne.x + 50 && x >= BombOne.x) || (x >= BombTwo.x - 50 && x <= BombTwo.x) || (x <= BombTwo.x + 50 && x >= BombTwo.x))
+		if ((x >= sdlProperties::BombOne.x - 50 && x <= sdlProperties::BombOne.x) || (x <= sdlProperties::BombOne.x + 50 && x >= sdlProperties::BombOne.x) || (x >= sdlProperties::BombTwo.x - 50 && x <= sdlProperties::BombTwo.x) || (x <= sdlProperties::BombTwo.x + 50 && x >= sdlProperties::BombTwo.x))
 		{
 			goto randomagain;
 		}
 		else {
-			point.x = x;
+			sdlProperties::point.x = x;
 		}
 		
-		point.y = y;
-		PosOfPoint = point.y;
+		sdlProperties::point.y = y;
+		PosOfPoint = sdlProperties::point.y;
 
 	}
 }
 bool FindCollisionOfBombOne() 
 {
-	if (((round(BombOne.y) - round(diver.y) >= -35) && (round(BombOne.y) - round(diver.y) <= 35) && (round(BombOne.x) - round(diver.x) <= 20) && (round(BombOne.x) - round(diver.x) >= -20)))
+	if (((round(sdlProperties::BombOne.y) - round(sdlProperties::diver.y) >= -35) && (round(sdlProperties::BombOne.y) - round(sdlProperties::diver.y) <= 35) && (round(sdlProperties::BombOne.x) - round(sdlProperties::diver.x) <= 20) && (round(sdlProperties::BombOne.x) - round(sdlProperties::diver.x) >= -20)))
 	{
 		return true;
 		
-		if (round(BombOne.y) - round(diver.y) == -35 && round(BombOne.x) - round(diver.x) >= -17)
+		if (round(sdlProperties::BombOne.y) - round(sdlProperties::diver.y) == -35 && round(sdlProperties::BombOne.x) - round(sdlProperties::diver.x) >= -17)
 		{
 			return true;
 			
@@ -515,11 +523,11 @@ bool FindCollisionOfBombOne()
 }
 bool FindCollisionOfBombTwo()
 {
-	if (((round(BombTwo.y) - round(diver.y) >= -35) && (round(BombTwo.y) - round(diver.y) <= 35) && (round(BombTwo.x) - round(diver.x) <= 20) && (round(BombTwo.x) - round(diver.x) >= -20)))
+	if (((round(sdlProperties::BombTwo.y) - round(sdlProperties::diver.y) >= -35) && (round(sdlProperties::BombTwo.y) - round(sdlProperties::diver.y) <= 35) && (round(sdlProperties::BombTwo.x) - round(sdlProperties::diver.x) <= 20) && (round(sdlProperties::BombTwo.x) - round(sdlProperties::diver.x) >= -20)))
 	{
 	return true;
 	
-	if (round(BombTwo.y) - round(diver.y) == -35 && round(BombTwo.x) - round(diver.x) >= -17)
+	if (round(sdlProperties::BombTwo.y) - round(sdlProperties::diver.y) == -35 && round(sdlProperties::BombTwo.x) - round(sdlProperties::diver.x) >= -17)
 	{
 		return true;
 		
@@ -528,27 +536,27 @@ bool FindCollisionOfBombTwo()
 }
 void RandomPosOfBombs()
 {
-	if (FindCollisionOfBombOne() || BombOne.y < -60 || BombTwo.y < -60 || FindCollisionOfBombTwo())
+	if (FindCollisionOfBombOne() || sdlProperties::BombOne.y < -60 || sdlProperties::BombTwo.y < -60 || FindCollisionOfBombTwo())
 	{
 		
 		
-		if (BombOne.y < -60)
+		if (sdlProperties::BombOne.y < -60)
 		{
 			
 			x = rand() % 520 + 55;
 			
 
-			BombOne.x = x;
+			sdlProperties::BombOne.x = x;
 			
 			
 
 		}
-		else if (BombTwo.y < -60)
+		else if (sdlProperties::BombTwo.y < -60)
 		{
 			
 			x = rand() % 520 + 55;
 
-			BombTwo.x = x;
+			sdlProperties::BombTwo.x = x;
 			
 		}
 		else if(FindCollisionOfBombOne())
@@ -558,9 +566,9 @@ void RandomPosOfBombs()
 				gameover -= 100;
 				x = rand() % 520 + 55;
 				y = rand() % 20 + 500;
-				BombOne.y = y;
-				BombOne.x = x;
-				PosYOfBombOne = BombOne.y;
+				sdlProperties::BombOne.y = y;
+				sdlProperties::BombOne.x = x;
+				PosYOfBombOne = sdlProperties::BombOne.y;
 			
 			
 				
@@ -575,9 +583,9 @@ void RandomPosOfBombs()
 			gameover -= 100;
 			x = rand() % 520 + 55;
 			y = rand() % 20 + 500;
-			BombTwo.y = y;
-			BombTwo.x = x;
-			PosYOfBombTwo = BombTwo.y;
+			sdlProperties::BombTwo.y = y;
+			sdlProperties::BombTwo.x = x;
+			PosYOfBombTwo = sdlProperties::BombTwo.y;
 		}
 		
 		
@@ -593,21 +601,21 @@ void RandomPosOfBombs()
 void MainLoop()
 {
 
-	LeftPosOfDiver = diver.x;
-	PosOfPoint = point.y;
-	RightPosOfDiver = diver.x;
-	PosYOfBombOne = BombOne.y;
-	PosYOfBombTwo = BombTwo.y;
-	BubbleLeftY = bubbleLeft.y;
-	BubbleRightY = bubbleRight.y;
+	LeftPosOfDiver = sdlProperties::diver.x;
+	PosOfPoint = sdlProperties::point.y;
+	RightPosOfDiver = sdlProperties::diver.x;
+	PosYOfBombOne = sdlProperties::BombOne.y;
+	PosYOfBombTwo = sdlProperties::BombTwo.y;
+	BubbleLeftY = sdlProperties::bubbleLeft.y;
+	BubbleRightY = sdlProperties::bubbleRight.y;
 
 	while (playing)
 	{
 
-		SDL_PollEvent(&SDLevent);
+		SDL_PollEvent(&sdlProperties::SDLevent);
 
 
-		if (SDLevent.type == SDL_QUIT)
+		if (sdlProperties::SDLevent.type == SDL_QUIT)
 		{
 
 			cout << endl << "GoodBye";
@@ -625,43 +633,43 @@ void MainLoop()
 		PosYOfBombTwo -= 0.2;
 		BubbleLeftY -= 0.15;
 		BubbleRightY -= 0.08;
-		bubbleLeft.y = BubbleLeftY;
-		point.y = PosOfPoint;
-		BombOne.y = PosYOfBombOne;
-		BombTwo.y = PosYOfBombTwo;
-		bubbleRight.y = BubbleRightY;
+		sdlProperties::bubbleLeft.y = BubbleLeftY;
+		sdlProperties::point.y = PosOfPoint;
+		sdlProperties::BombOne.y = PosYOfBombOne;
+		sdlProperties::BombTwo.y = PosYOfBombTwo;
+		sdlProperties::bubbleRight.y = BubbleRightY;
 
 
-		if (bubbleLeft.y < -60)
+		if (sdlProperties::bubbleLeft.y < -60)
 		{
 			BubbleLeftY = 500;
 
 		}
-		else if (bubbleRight.y < -60)
+		else if (sdlProperties::bubbleRight.y < -60)
 		{
 			BubbleRightY = 500;
 
 		}
-		else if (BombOne.y < -60)
+		else if (sdlProperties::BombOne.y < -60)
 		{
 			PosYOfBombOne = 500;
 		}
-		else if (BombTwo.y < -60)
+		else if (sdlProperties::BombTwo.y < -60)
 		{
 			PosYOfBombTwo = 500;
 		}
-		else if (diver.x < -10)
+		else if (sdlProperties::diver.x < -10)
 		{
-			diver.x = 700;
-			LeftPosOfDiver = diver.x;
-			RightPosOfDiver = diver.x;
+			sdlProperties::diver.x = 700;
+			LeftPosOfDiver = sdlProperties::diver.x;
+			RightPosOfDiver = sdlProperties::diver.x;
 
 		}
-		else if (diver.x > 710)
+		else if (sdlProperties::diver.x > 710)
 		{
-			diver.x = 0;
-			LeftPosOfDiver = diver.x;
-			RightPosOfDiver = diver.x;
+			sdlProperties::diver.x = 0;
+			LeftPosOfDiver = sdlProperties::diver.x;
+			RightPosOfDiver = sdlProperties::diver.x;
 		}
 
 		//Find collision and set random position of the point
